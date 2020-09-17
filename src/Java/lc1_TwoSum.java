2f1bb24010193637c20186999217f7cff3bdc38c
VictorChan <achenxuanjun@gmail.com>
2020-09-17 18:08:28 +0800

    lc1_TwoSum

---
+++ src/Java/lc1_TwoSum.java
@@ -0,0 +1,16 @@
+package Java;
+import java.util.*;
+
+public class lc1_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            if (map.containsKey(target - x)) {
+                return new int[]{map.get(target - x), i};
+            }
+            map.put(x, i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
