f2777c886912c1832517ba5ea07517a263433671
VictorChan1 <achenxuanjun@gmail.com>
2020-09-18 23:36:19 +0800

    two sum draft

--- src/Java/lc1_TwoSum.java
+++ src/Java/lc1_TwoSum.java
@@ -3,14 +3,35 @@ import java.util.*;
 
 public class lc1_TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int x = nums[i];
-            if (map.containsKey(target - x)) {
-                return new int[]{map.get(target - x), i};
+        if (nums == null) return null;
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum == target) {
+                map.put("max",i+1);
+                map.put("min",j+1);
+                return map;
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
             }
-            map.put(x, i);
         }
-        throw new IllegalArgumentException("No two sum solution");
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[4];
+        nums[0] = 2;
+        nums[1] = 7;
+        nums[2] = 11;
+        nums[3] = 15;
+        int target = 9;
+        res = twoSum(nums, target);
+        System.out.println("22");
     }
 }
